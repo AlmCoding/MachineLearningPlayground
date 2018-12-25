ddc7041887b027ecc5ccfa57ec4af0e688390d9b
AlmCoding <alexander@lechthaler.online>
2018-12-25 11:09:53 +0100

    Complete part 1 and part 2 of exercise 1

--- SingleNeuron/single_neuron12.py
+++ SingleNeuron/single_neuron12.py
@@ -1,5 +1,5 @@
 """
-    Single Neuron Linear Regression
+    Single Neuron Linear Regression with data normalization
     N = #Samples
     i = [0, ... ,N]
     M = Dimension of samples +1(offset b)
@@ -13,13 +13,28 @@ import numpy as np
 from dataset1_linreg import DataSet
 
 
+normalize_data = False
 y_D, x_D = DataSet.get_data()
 DataSet.plot_data()
 # Initialize weights
 w = np.array([[0.1], [0.1]])
 
 
+def get_norm_params(x_arr):
+    mean_x = np.sum(x_arr)/x_arr.size
+    xmm = x_arr - mean_x
+    stdd_x = np.sqrt(np.sum(xmm**2)/x_arr.size)
+    return mean_x, stdd_x
+
+
+mean, stdd = get_norm_params(x_D)
+
+
 def predict_y1(x_arr):
+    if normalize_data:
+        # Normalize Input Samples
+        x_arr = (x_arr - mean) / stdd
+
     a = w[1][0] * x_arr + w[0][0]
     return a
 
@@ -48,9 +63,12 @@ grad = gradient_w(x_D, y_D, predict_y1)
 print("Initial gradient:{}".format(grad))
 
 # Weight tuning
-for i in range(1000):
+i = 0
+while l2_cost(x_D, y_D, predict_y1) > 0.9:
     w = w - 0.01 * gradient_w(x_D, y_D, predict_y1)
+    i += 1
 
 DataSet.plot_model(predict_y1)
-print('Final Cost:%f' % l2_cost(x_D, y_D, predict_y1))
+print('Final Cost: %f' % l2_cost(x_D, y_D, predict_y1))
+print("Iteration needed: {}".format(i))
 
